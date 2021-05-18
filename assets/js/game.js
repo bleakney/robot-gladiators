5f1ac24b2eecbc39599f8bf57e06b987be720c93
Bleakney Bissett <bleakneybissett@gmail.com>
2021-05-18 16:31:30 -0500

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");}
+    // fight();
+
